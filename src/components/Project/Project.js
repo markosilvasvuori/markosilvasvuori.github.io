fb9288726840b7bee9b4c23715ae03d537da010f
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-06-01 15:52:57 +0300

    Moved image-container out from content div

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,10 +4,10 @@ import buttonStyles from '../UI/Button.module.scss';
 const Project = ({ imgSrc, title, technologies, description, liveLink, gitHubLink }) => {
     return (
         <div className={classes.project}>
-            <div className={classes.content}>
             <div className={classes['image-container']}>
                 <img src={imgSrc} alt={'title'} />
             </div>
+            <div className={classes.content}>
                 <h3>{title}</h3>
                 <ul>
                     {technologies.map((tech, index) => (
@@ -20,24 +20,24 @@ const Project = ({ imgSrc, title, technologies, description, liveLink, gitHubLin
                     ))}
                 </ul>
                 <p>{description}</p>
-            </div>
-            <div className={classes.buttons}>
-                <a 
-                    className={buttonStyles.button} 
-                    href={liveLink}
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    Live Site
-                </a>
-                <a 
-                    className={`${buttonStyles.button} ${buttonStyles.outlined}`} 
-                    href={gitHubLink}
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    GitHub
-                </a>
+                <div className={classes.buttons}>
+                    <a 
+                        className={buttonStyles.button} 
+                        href={liveLink}
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        Live Site
+                    </a>
+                    <a 
+                        className={`${buttonStyles.button} ${buttonStyles.outlined}`} 
+                        href={gitHubLink}
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        GitHub
+                    </a>
+                </div>
             </div>
         </div>
     );
